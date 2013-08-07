63d36415bc39ede7c606085c86ead2779f267314
Philippe Bruhat (BooK) <book@cpan.org>
2013-08-07 18:29:59 +0200

    Git::Simple::Role::Object - a role for Git objects
    For the time being, we're not trying to be as memory-efficient
    as Glow is. The content of an object will be loaded in memory
    in about every case.

---
+++ lib/Git/Simple/Role/Object.pm
@@ -0,0 +1,39 @@
+package Git::Simple::Role::Object;
+
+use Moo::Role;
+use Sub::Quote;
+
+requires qw( kind );
+
+has repository => (
+    is       => 'ro',
+    required => 1,
+);
+
+has digest => (
+    is  => 'lazy',
+);
+
+# size in bytes
+has size => (
+    is      => 'lazy',
+    default => sub { length $_[0]->content },
+);
+
+has content => (
+    is => 'lazy',
+);
+
+# to compute the digest of a new object, save it in the object database
+sub _build_digest {
+    my ($self) = @_;
+    return
+        scalar $self->repository->run( 'hash-object', '-t', $self->kind,
+        '--stdin', '-w', { input => $self->content } );
+}
+
+1;
+
+__END__
+
+# ABSTRACT: Role for objects from the Git object database
