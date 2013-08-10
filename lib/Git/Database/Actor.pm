7ab20871d1555c90efc87ec0d33d757f1194e880
Philippe Bruhat (BooK) <book@cpan.org>
2013-08-10 03:59:26 +0200

    Git::Database::Actor for the people!

---
+++ lib/Git/Database/Actor.pm
@@ -0,0 +1,61 @@
+package Git::Database::Actor;
+
+use Moo;
+
+has name => (
+    is       => 'ro',
+    required => 1,
+);
+
+has email => (
+    is       => 'ro',
+    required => 1,
+);
+
+# private attributes
+has ident => (
+    is       => 'lazy',
+    init_arg => undef,
+);
+
+sub _build_ident { $_[0]->name . ' <' . $_[0]->email . '>' }
+
+1;
+
+__END__
+
+# ABSTRACT: An actor in Git::Database
+
+=head1 SYNOPSIS
+
+    use Git::Database::Actor;
+
+    my $actor = Git::Database::Actor->new(
+        name  => 'Philippe Bruhat (BooK)',
+        email => 'book@cpan.org'
+    );
+
+    print $actor->ident;    # Philippe Bruhat (BooK) <book@cpan.org>
+
+=head1 DESCRIPTION
+
+L<Git::Database::Actor> represents a user in L<Git::Database>,
+i.e. the combination of a name and an email.
+
+=head1 ATTRIBUTES
+
+=head2 name
+
+The name of the actor.
+
+=head2 email
+
+The email of the actor.
+
+=head2 ident
+
+The identity of the actor, build as:
+
+    Name <email>
+
+=cut
