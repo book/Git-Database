ccf7a9da2418f2151b35e7d35e42496ab599ecc7
Philippe Bruhat (BooK) <book@cpan.org>
2013-10-22 09:08:49 +0200

    documentation for Git::Database::Object::Tag

--- lib/Git/Database/Object/Tag.pm
+++ lib/Git/Database/Object/Tag.pm
@@ -77,3 +77,100 @@ sub _build_content {
 }
 
 1;
+
+# ABSTRACT: A tag object in the Git database
+
+=head1 SYNOPSIS
+
+    my $r   = Git::Database->new();       # current Git repository
+    my $tag = $r->get_object('f5c10c');   # abbreviated digest
+
+    # attributes
+    $tag->kind;              # tag
+    $tag->digest;            # f5c10c1a841419d3b1db0c3e0c42b554f9e1eeb2
+    $tag->object;            # ef25e81ba86b7df16956c974c8a9c1ff2eca1326
+    $tag->type;              # commit
+    ...;                     # etc., see below
+
+=head1 DESCRIPTION
+
+Git::Database::Object::Tag represents a C<tag> object
+obtained via L<Git::Database> from a Git object database.
+
+=head1 ATTRIBUTES
+
+=head2 kind
+
+The object kind: C<tag>
+
+=head2 digest
+
+The SHA-1 digest of the digest object.
+
+=head2 content
+
+The object's actual content.
+
+=head2 size
+
+The size (in bytes) of the object content.
+
+=head2 tag_info
+
+A hash reference containing the all the attributes listed below, as
+values for the keys with the same names.
+
+=head2 object
+
+The SHA-1 digest of the tagged object.
+
+=head2 type
+
+The type of the tagged object.
+
+=head2 tag
+
+The tag name.
+
+=head2 tagger
+
+A L<Git::Database::Actor> object representing the author of
+the tag.
+
+=head2 tagged_time
+
+A L<DateTime> object representing the date at which the author
+created the tag.
+
+=head2 comment
+
+The text of the tag.
+
+=head1 METHODS
+
+=head2 new()
+
+Create a new Git::Object::Database::Tag object.
+
+One (and only one) of the C<content> or C<tag> arguments is
+required.
+
+C<tag_info> is a reference to a hash containing the keys listed
+above, i.e.  C<object>, C<type>, C<tag>, C<tagger>, C<tagged_time>,
+and C<comment>.
+
+=head1 SEE ALSO
+
+L<Git::Database>,
+L<Git::Database::Role::Object>.
+
+=head1 COPYRIGHT
+
+Copyright 2013 Philippe Bruhat (BooK), all rights reserved.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
