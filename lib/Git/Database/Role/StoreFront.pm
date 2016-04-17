88199b1ee2298a63c9ae648c34f5316fb60d3134
Philippe Bruhat (BooK) <book@cpan.org>
2016-04-17 17:53:48 +0200

    Document has_object and get_object from Git::Database::Role::StoreFront

--- lib/Git/Database/Role/StoreFront.pm
+++ lib/Git/Database/Role/StoreFront.pm
@@ -8,3 +8,40 @@ requires
 ;
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Git::Database::Role::StoreFront - Role for a Git data store frontend
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 has_object
+
+    if ( $store->has_object($digest) ) {
+        ...;
+    }
+
+Returns a boolean value indicating if the given digest is available in
+this store. If true, the returned value will be equal to the object kind
+(C<blob>, C<tree>, C<commit> or C<tag>).
+
+=head2 get_object
+
+    my $object = $store->get_object($digest);
+
+Return an object doing the L<Git::Database::Role::Object> role, or
+C<undef> if the store does not contain an object for the given digest.
+
+If the given digest is a "short SHA1" and it is ambiguous (matches more
+than one object), no object will be returned. Depending on the actual
+store class, a warning might be emitted.
+
+=cut
