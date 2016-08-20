a53cb80128286ab75196852f5a4fd99e59603472
Philippe Bruhat (BooK) <book@cpan.org>
2016-08-21 00:42:58 +0200

    Refactor Git::Database
    Git::Database now has a 'backend' attribute to which most of the
    operations related to Git are delegated.

--- lib/Git/Database.pm
+++ lib/Git/Database.pm
@@ -1,103 +1,32 @@
 package Git::Database;
 
-use Moo;
 use Sub::Quote;
+use Module::Runtime qw( use_module );
 
-extends 'Git::Repository';
+use Moo;
+use namespace::clean;
 
-has _object_factory => (
-    is  => 'lazy',
-    isa => quote_sub(
-        q{ die 'Not a Git::Repository::Command' if ! $_[0]->isa('Git::Repository::Command' ) }
-    ),
-    required => 0,
-    predicate => 1,
+has backend => (
+    is       => 'ro',
+    required => 1,
+    default  => sub { use_module('Git::Database::Backend::None')->new; },
+    isa      => sub {
+        die "$_[0] DOES not Git::Database::Role::Backend"
+          if !eval { $_[0]->does('Git::Database::Role::Backend') };
+    },
+    handles => [
+        'hash_object',        # Git::Database::Role::Backend
+        'get_object_meta',    # Git::Database::Role::ObjectReader
+        'get_object',
+        'get_hashes',
+        'has_object',
+        'put_object',         # Git::Database::Role::ObjectWriter
+        'resolve_ref',        # Git::Database::Role::RefReader
+        'get_refs',
+        'put_ref',            # Git::Database::Role::RefWriter
+        'delete_ref'
+    ],
 );
-
-sub _build__object_factory { $_[0]->command( 'cat-file', '--batch' ); }
-
-has _object_checker => (
-    is  => 'lazy',
-    isa => quote_sub(
-        q{ die 'Not a Git::Repository::Command' if ! $_[0]->isa('Git::Repository::Command' ) }
-    ),
-    required  => 0,
-    predicate => 1,
-);
-
-sub _build__object_checker { $_[0]->command( 'cat-file', '--batch-check' ); }
-
-use Git::Database::Object::Blob;
-use Git::Database::Object::Tree;
-use Git::Database::Object::Commit;
-use Git::Database::Object::Tag;
-my %kind2class = (
-    blob   => 'Git::Database::Object::Blob',
-    tree   => 'Git::Database::Object::Tree',
-    commit => 'Git::Database::Object::Commit',
-    tag    => 'Git::Database::Object::Tag',
-);
-
-sub get_object {
-    my ( $self, $digest ) = @_;
-    my $factory = $self->_object_factory;
-
-    # request the object
-    print { $factory->stdin } $digest, "\n";
-
-    # process the reply
-    my $out = $factory->stdout;
-    local $/ = "\012";
-    chomp( my $reply = <$out> );
-    my ( $sha1, $kind, $size ) = split / /, $reply;
-
-    # object does not exist in the git object database
-    return if $kind eq 'missing';
-
-    # read the whole content in memory at once
-    my $res = read $out, (my $content), $size;
-    if( $res != $size ) {
-         $factory->close; # in case the exception is trapped
-         die "Read $res/$size of content from git";
-    }
-
-    # read the last byte
-    $res = read $out, (my $junk), 1;
-    if( $res != 1 ) {
-         $factory->close; # in case the exception is trapped
-         die "Unable to finish reading content from git";
-    }
-
-    # careful with utf-8!
-    # create a new object with digets, content and size
-    return $kind2class{$kind}->new(
-        repository => $self,
-        size       => $size,
-        content    => $content,
-        digest     => $sha1
-    );
-}
-
-sub has_object {
-    my ( $self, $digest ) = @_;
-    my $checker = $self->_object_checker;
-
-    # request the object
-    print { $checker->stdin } $digest, "\n";
-
-    # process the reply
-    local $/ = "\012";
-    chomp( my $reply = $checker->stdout->getline );
-    my ( $sha1, $kind, $size ) = split / /, $reply;
-
-    return wantarray ? ( $sha1, $kind, $size ) : defined $size;
-}
-
-sub DEMOLISH {
-    my ($self) = @_;
-    $self->_object_factory->close if $self->_has_object_factory;
-    $self->_object_checker->close if $self->_has_object_checker;
-}
 
 1;
 
