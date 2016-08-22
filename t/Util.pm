781196dd613a009c33836c8a5be9914d8020078b
Philippe Bruhat (BooK) <book@cpan.org>
2016-08-22 23:49:08 +0200

    Use * instead of - as a bullet for object tests

--- t/Util.pm
+++ t/Util.pm
@@ -71,7 +71,7 @@ my %test_for = (
                 sort { $a->filename cmp $b->filename }
                   @{ $test->{directory_entries} }
             ],
-            '- directory_entries'
+            '* directory_entries'
         );
     },
     commit => sub {
@@ -83,20 +83,20 @@ my %test_for = (
             is(
                 $commit_info->{$attr},
                 $test->{commit_info}{$attr},
-                "- commit_info $attr"
+                "* commit_info $attr"
             );
         }
         for my $attr (qw( author committer )) {
             is(
                 $commit_info->{$attr}->ident,
                 $test->{commit_info}{$attr}->ident,
-                "- commit_info $attr"
+                "* commit_info $attr"
             );
         }
         is(
             join( ' ', @{ $commit_info->{parents_digest} } ),
             join( ' ', @{ $test->{commit_info}{parents_digest} || [] } ),
-            '- commit_info parents_digest'
+            '* commit_info parents_digest'
         );
     },
     tag => sub {
@@ -106,12 +106,12 @@ my %test_for = (
         my $tag_info = $tag->tag_info;
         for my $attr (qw( object type tag tagged_time comment )) {
             is( $tag_info->{$attr}, $test->{tag_info}{$attr},
-                "- tag_info $attr" );
+                "* tag_info $attr" );
         }
         is(
             $tag_info->{tagger}->ident,
             $test->{tag_info}{tagger}->ident,
-            '- tag_info tagger'
+            '* tag_info tagger'
         );
     },
 );
@@ -124,11 +124,11 @@ sub test_object {
     isa_ok( $object, "Git::Database::Object::\u$kind" );
 
     # read content in memory early
-    is( $object->kind,      $test->{kind},    '- kind' );
-    is( $object->content,   $test->{content}, '- content' );
-    is( $object->size,      $test->{size},    '- size' );
-    is( $object->digest,    $test->{digest},  '- digest' );
-    is( $object->as_string, $test->{string},  '- as_string' );
+    is( $object->kind,      $test->{kind},    '* kind' );
+    is( $object->content,   $test->{content}, '* content' );
+    is( $object->size,      $test->{size},    '* size' );
+    is( $object->digest,    $test->{digest},  '* digest' );
+    is( $object->as_string, $test->{string},  '* as_string' );
 
     # run the kind-specific tests
     $test_for{$kind}->( $object, $test ) if exists $test_for{$kind};
