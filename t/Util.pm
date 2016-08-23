0a0a98ac2fd6ca49d70c081917743187410910f3
Philippe Bruhat (BooK) <book@cpan.org>
2016-08-24 00:01:35 +0200

    Use = instead of * as a bullet for object tests

--- t/Util.pm
+++ t/Util.pm
@@ -110,7 +110,7 @@ my %cmp_for = (
                 sort { $a->filename cmp $b->filename }
                   @{ $test->{directory_entries} }
             ],
-            '* directory_entries'
+            '= directory_entries'
         );
     },
     commit => sub {
@@ -122,20 +122,20 @@ my %cmp_for = (
             is(
                 $commit_info->{$attr},
                 $test->{commit_info}{$attr},
-                "* commit_info $attr"
+                "= commit_info.$attr"
             );
         }
         for my $attr (qw( author committer )) {
             is(
                 $commit_info->{$attr}->ident,
                 $test->{commit_info}{$attr}->ident,
-                "* commit_info $attr"
+                "= commit_info.$attr"
             );
         }
         is(
             join( ' ', @{ $commit_info->{parents_digest} } ),
             join( ' ', @{ $test->{commit_info}{parents_digest} || [] } ),
-            '* commit_info parents_digest'
+            '= commit_info.parents_digest'
         );
     },
     tag => sub {
@@ -145,12 +145,12 @@ my %cmp_for = (
         my $tag_info = $tag->tag_info;
         for my $attr (qw( object type tag tagged_time comment )) {
             is( $tag_info->{$attr}, $test->{tag_info}{$attr},
-                "* tag_info $attr" );
+                "= tag_info.$attr" );
         }
         is(
             $tag_info->{tagger}->ident,
             $test->{tag_info}{tagger}->ident,
-            '* tag_info tagger'
+            '= tag_info.tagger'
         );
     },
 );
@@ -163,11 +163,11 @@ sub cmp_git_objects {
     isa_ok( $object, "Git::Database::Object::\u$kind" );
 
     # read content in memory early
-    is( $object->kind,      $test->{kind},    '* kind' );
-    is( $object->content,   $test->{content}, '* content' );
-    is( $object->size,      $test->{size},    '* size' );
-    is( $object->digest,    $test->{digest},  '* digest' );
-    is( $object->as_string, $test->{string},  '* as_string' );
+    is( $object->kind,      $test->{kind},    "= kind: $test->{kind}" );
+    is( $object->content,   $test->{content}, '= content' );
+    is( $object->size,      $test->{size},    '= size' );
+    is( $object->digest,    $test->{digest},  "= digest: $test->{digest}" );
+    is( $object->as_string, $test->{string},  '= as_string' );
 
     # run the kind-specific tests
     $cmp_for{$kind}->( $object, $test ) if exists $cmp_for{$kind};
