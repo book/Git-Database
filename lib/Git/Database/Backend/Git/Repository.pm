3d096992b09f61257a997d120ec3892f02e68459
Philippe Bruhat (BooK) <book@cpan.org>
2016-08-26 00:04:08 +0200

    Add some basic documentation for Git::Database::Backend::Git::Repository

--- lib/Git/Database/Backend/Git/Repository.pm
+++ lib/Git/Database/Backend/Git/Repository.pm
@@ -39,3 +39,41 @@ sub DEMOLISH {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Git::Database::Backend::Git::Repository - A Git::Database backend based on Git::Repository
+
+=head1 SYNOPSIS
+
+    # get a store
+    my $r  = Git::Repository->new();
+
+    # provide the backend
+    my $b  = Git::Database::Backend::Git::Repository->new( store => $r );
+    my $db = Git::Database->new( backend => $b );
+
+    # let Git::Database figure it out by itself
+    my $db = Git::Database->new( store => $r );
+
+=head1 DESCRIPTION
+
+This backend reads and write data from a Git repository using the
+L<Git::Repository> Git wrapper.
+
+=head1 AUTHOR
+
+Philippe Bruhat (BooK) <book@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright 2013-2016 Philippe Bruhat (BooK), all rights reserved.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
